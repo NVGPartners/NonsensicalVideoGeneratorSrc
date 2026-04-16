dd63d0b9034668127c76042979b38bb566d0e26d
KiwifruitDev <teampopplio@protonmail.com>
2026-04-16 16:32:45 -0700

    Helping with v1.8.1.3

--- src/data/BlogData.cs
+++ src/data/BlogData.cs
@@ -8,12 +8,12 @@ namespace NonsensicalVideoGenerator
     public static class BlogData
     {
         public static readonly int MinorUpcoming = 8;
-        public static readonly int MajorUpcoming = 2;
-        public static readonly int Month = 8;
-        public static readonly int Day = 27;
-        public static readonly int Year = 2025;
-        public static readonly int Tier = 2;
-        public static readonly string PostId = "654821763669034264";
+        public static readonly int MajorUpcoming = 3;
+        public static readonly int Month = 4;
+        public static readonly int Day = 16;
+        public static readonly int Year = 2026;
+        public static readonly int Tier = 1; // 0: Upcoming, 1: Minor Update, 2: Major Update
+        public static readonly string PostId = "000000000000000000"; // Fill in with update post ID before publishing the build
         public static List<string> Subtitle { get; set; } = new List<string>()
         {
             "v%version%",
@@ -23,10 +23,10 @@ namespace NonsensicalVideoGenerator
         public static List<string> Description { get; set; } = new List<string>()
         {
             // "WWWWWWWWWWWWWWWWWWWWWWWWWWWW"
-            "Interim update",
-            " ",
-            " ",
-            " ",
+            "- Updated dependencies",
+            "- Fixed yt-dlp search bug",
+            "- Added addon ability for",
+            "several random library files",
             " ",
             " ",
             " ",
